a07d3387423dc674282afd2b7e10e9d08887f3ce
Matheus Fontana <mbfontana@outlook.com>
2023-03-28 18:06:11 -0300

    Task to process user input via serial monitor

--- freeRTOS_approach/energy_monitoring.ino
+++ freeRTOS_approach/energy_monitoring.ino
@@ -4,8 +4,46 @@
 #define WIFI_SSID "Fontana"
 #define WIFI_PASSWORD "@678fontana786"
 
+// Core definition
+static const BaseType_t pro_cpu = 0;
+static const BaseType_t app_cpu = 1;
+
 HTTPClient httpClient;
 WiFiClient wifiClient;
+
+// Function prototypes
+void userInterfaceTask(void *pvParameters);
+void handleCommand(String command);
+
+// Tasks
+void userInterfaceTask(void *pvParameters)
+{
+    while (1)
+    {
+        if (Serial.available())
+        {
+            String command = Serial.readStringUntil('\n');
+            command.trim();
+            handleCommand(command);
+        }
+
+        // Wait for 10ms
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+// Procedures
+void handleCommand(String command)
+{
+    if (command == "hello")
+    {
+        Serial.println("Hello, World!");
+    }
+    else
+    {
+        Serial.println("Invalid command.");
+    }
+}
 
 void setup()
 {
@@ -24,6 +62,9 @@ void setup()
     }
     Serial.println("Connected to WiFi");
     Serial.println("ESP32 IP address: %d\n", WiFi.localIP());
+
+    // Task to interact with user via serial monitor
+    xTaskCreatePinnedToCore(userInterfaceTask, "UserInterface", 2048, NULL, 1, NULL, app_cpu);
 }
 
 void loop()
