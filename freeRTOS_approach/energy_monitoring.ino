1e314e81e058db37200f2412d9844749e290eecd
Matheus Fontana <mbfontana@outlook.com>
2023-03-29 07:20:04 -0300

    Post request to insert sample data

--- freeRTOS_approach/energy_monitoring.ino
+++ freeRTOS_approach/energy_monitoring.ino
@@ -1,19 +1,20 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-#define WIFI_SSID "Fontana"
-#define WIFI_PASSWORD "@678fontana786"
+// Credentials
+const char *wifiSSID = "Fontana";
+const char *wifiPassword = "@678fontana786";
+const char *serverUrl = "http://192.168.0.156";
+const int serverPort = 3001;
 
 // Core definition
 static const BaseType_t pro_cpu = 0;
 static const BaseType_t app_cpu = 1;
 
-HTTPClient httpClient;
-WiFiClient wifiClient;
-
 // Function prototypes
-void userInterfaceTask(void *pvParameters);
+void sendPostRequest(String endpoint, String body);
 void handleCommand(String command);
+void userInterfaceTask(void *pvParameters);
 
 // Tasks
 void userInterfaceTask(void *pvParameters)
@@ -39,9 +40,87 @@ void handleCommand(String command)
     {
         Serial.println("Hello, World!");
     }
+    else if (command == "post_request")
+    {
+        String json = "";
+        json += "{";
+        json += "\"fft01\": 10.8,";
+        json += "\"fft03\": 10.8,";
+        json += "\"fft05\": 10.8,";
+        json += "\"fft07\": 10.8,";
+        json += "\"fft09\": 10.8,";
+        json += "\"fft11\": 10.8,";
+        json += "\"fft13\": 10.8,";
+        json += "\"fft15\": 10.8,";
+        json += "\"fft17\": 10.8,";
+        json += "\"fft19\": 10.8,";
+        json += "\"fft20\": 10.8,";
+        json += "\"fft21\": 10.8,";
+        json += "\"fft23\": 10.8,";
+        json += "\"fft25\": 10.8,";
+        json += "\"fft27\": 10.8,";
+        json += "\"fft29\": 10.8,";
+        json += "\"fft31\": 10.8,";
+        json += "\"applianceId\": 1";
+        json += "}";
+
+        Serial.println("JSON: ");
+        Serial.println(json);
+
+        sendPostRequest("/sample", json);
+    }
     else
     {
         Serial.println("Invalid command.");
+    }
+}
+
+void sendPostRequest(String endpoint, String body)
+{
+    HTTPClient http;
+
+    String webServer = "";
+    webServer += serverUrl;
+    webServer += ":";
+    webServer += serverPort;
+    webServer += endpoint;
+
+    Serial.println("Connecting to server...");
+    if (http.begin(webServer))
+    {
+        Serial.println("Connected to server");
+
+        // Set the content type header
+        http.addHeader("Content-Type", "application/json");
+
+        // Send the POST request with the JSON body
+        Serial.print("Making a POST request to: ");
+        Serial.print(serverUrl);
+        Serial.print(":");
+        Serial.print(serverPort);
+        Serial.print(endpoint);
+
+        int httpResponseCode = http.POST(body);
+
+        // Check if the request was successful
+        if (httpResponseCode > 0)
+        {
+            // Print the response payload
+            Serial.println(http.getString());
+        }
+        else
+        {
+            Serial.print("Error sending HTTP request: ");
+            Serial.println(http.getString());
+        }
+
+        // Close the connection
+        http.end();
+        Serial.println("Connection closed");
+    }
+    else
+    {
+        Serial.println("Connection failed");
     }
 }
 
@@ -54,17 +133,25 @@ void setup()
     Serial.println("\n--- Energy Monitoring ESP32 ---\n");
 
     // Connect to WiFi
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    WiFi.begin(wifiSSID, wifiPassword);
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(1000);
-        Serial.println("Connecting to WiFi...");
+        Serial.println("[WiFi] Connecting to WiFi...");
     }
-    Serial.println("Connected to WiFi");
-    Serial.println("ESP32 IP address: %d\n", WiFi.localIP());
+    Serial.println("[WiFi] Connected to WiFi");
+
+    // Print the SSID of the network
+    Serial.print("[WiFi] SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // Print WiFi shield's IP address
+    IPAddress ip = WiFi.localIP();
+    Serial.print("[WiFi] IP Address: ");
+    Serial.println(ip);
 
     // Task to interact with user via serial monitor
-    xTaskCreatePinnedToCore(userInterfaceTask, "UserInterface", 2048, NULL, 1, NULL, app_cpu);
+    xTaskCreatePinnedToCore(userInterfaceTask, "UserInterface", 8000, NULL, 1, NULL, app_cpu);
 }
 
 void loop()
